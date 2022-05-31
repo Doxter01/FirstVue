7ae16749f6f86a973cd8a93781e048f8ed347c9c
Dorian <dorian.ku@gmail.com>
2022-05-31 23:08:37 +0200

    Installed vue

---
+++ myapp/src/views/KontaktView.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>Ma nemoooooooooooooooooj</p>
+</template>
+
+<script>
+export default {
+  name: "KontaktView",
+  data() {
+    return {
+      ime: "Dorian",
+    };
+  },
+};
+</script>
+
+<style></style>
